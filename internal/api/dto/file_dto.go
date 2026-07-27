9faefcd330081582b9f5e9564b3a40bde2fa2308
hrithik-shah <hrithik21.shah@gmail.com>
2026-07-27 10:31:14 +0400

    Add generic file repsonse object

--- internal/api/dto/file_dto.go
+++ internal/api/dto/file_dto.go
@@ -1,5 +1,11 @@
 package dto
 
+type FileInfo struct {
+	ID          string `json:"id" validate:"required,uuid"`
+	Name        string `json:"name" validate:"required,min=1"`
+	DirectoryID string `json:"directory_id" validate:"required,uuid"`
+}
+
 type UpdateFileRequest struct {
 	Name string `json:"name" validate:"required,min=1"`
 }
