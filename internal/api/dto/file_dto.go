b808fce7c36ee3bfeb9f8f3cf8561106088b613f
hrithik-shah <hrithik21.shah@gmail.com>
2026-07-28 14:06:21 +0400

    Add toFileDTO helper

--- internal/api/dto/file_dto.go
+++ internal/api/dto/file_dto.go
@@ -1,8 +1,19 @@
 package dto
 
+import "gosync/internal/models"
+
 type FileDTO struct {
 	ID          string
 	Name        string
 	DirectoryID string
 	Hash        string
+}
+
+func ToFileDTO(f *models.File) FileDTO {
+	return FileDTO{
+		ID:          f.ID.String(),
+		Name:        f.Name,
+		DirectoryID: f.DirectoryID.String(),
+		Hash:        f.ContentHash,
+	}
 }
